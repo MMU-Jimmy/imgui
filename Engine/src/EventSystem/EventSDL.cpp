a0a8cd72302553ca97c0f762e15aceae385868b1
MMU-Jimmy <j.mullin@mmu.ac.uk>
2024-08-13 13:13:04 +0100

    SDL Window Events Fix
    - Switch statement in EventSDL.cpp was not handling window events correctly - now hooked in.
    
    - Small typo in .gitignore rectified

--- Engine/src/EventSystem/EventSDL.cpp
+++ Engine/src/EventSystem/EventSDL.cpp
@@ -2,23 +2,30 @@
 
 void EventSDL::PollEventsSDL(SDL_Event e, Window::WindowData& windowData)
 {
-	switch (e.type)
-	{
-	case SDL_WINDOWEVENT_CLOSE:
-	{
-		Event_WindowClose closeEvent;
-		windowData.EventCallback(closeEvent);
-		break;
-	}
+    switch (e.type)
+    {
+    case SDL_WINDOWEVENT:
+        switch (e.window.event)
+        {
+        case SDL_WINDOWEVENT_CLOSE:
+        {
+            Event_WindowClose closeEvent;
+            windowData.EventCallback(closeEvent);
+            break;
+        }
+        default:
+            break;
+        }
+        break;
 
-	case SDL_MOUSEMOTION:
-	{
-		Event_MouseMoved event((double)e.motion.x, (double)e.motion.y);
-		windowData.EventCallback(event);
-		break;
-	}
+    case SDL_MOUSEMOTION:
+    {
+        Event_MouseMoved event((double)e.motion.x, (double)e.motion.y);
+        windowData.EventCallback(event);
+        break;
+    }
 
-	default:
-		break;	
-	}
+    default:
+        break;
+    }
 }
